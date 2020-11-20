1decaee33e521d04be56b3de8b1f7c42656f31b0
andry81 <andry@inbox.ru>
2020-11-20 06:52:26 +0000

    2020.11.20: * fixed: src/scripts/scm/shell: shell_copy_by_list.bat: reimplemented to bypass long 260+ path characters issue * fixed: src/scripts/scm/shell: shell_copy_by_list.bat: incorrect mkdir by msys/cygwin * new: src/scripts/scm/shell: shell_copy_by_list.bat: file/directory copy with rename support * changed: src/scripts/scm/shell: shell_copy_by_list.bat: ignore change directory call in case of long 260+ path characters issue
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklebar/tacklebar/trunk@43 b6e1d463-c974-416b-a8a8-9e453b4b0c2d

---
+++ src/scripts/scm/shell/.shell_copy_by_list/shell_copy_by_list.xcopy_file_with_rename.bat
@@ -0,0 +1,70 @@
+@echo off
+
+setlocal
+
+rem create an empty destination file if not exist yet to check a path limitation issue
+type nul >> "\\?\%TO_FILE_PATH%"
+
+if exist "%FROM_FILE_PATH%" if exist "%TO_FILE_PATH%" (
+  copy "%FROM_FILE_PATH%" "%TO_FILE_PATH%" /B /Y || exit /b 31
+  exit /b 0
+)
+
+rem rename through a temporary file
+mkdir "%COPY_WITH_RENAME_DIR_TMP%" 2>nul || robocopy.exe /create "%EMPTY_DIR_TMP%" "%COPY_WITH_RENAME_DIR_TMP%" >nul || (
+  echo.%?~nx0%: error: could not create temporary directory: "%COPY_WITH_RENAME_DIR_TMP%".
+  exit /b 40
+) >&2
+
+call :MAIN %%*
+set LASTERRORLEVEL=%ERRORLEVEL%
+
+rmdir /S /Q "\\?\%COPY_WITH_RENAME_DIR_TMP%" >nul 2>nul
+
+exit /b %LASTERRORLEVEL%
+
+:MAIN
+type nul >> "\\?\%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%"
+
+if not exist "%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%" (
+  echo.%?~nx0%: error: temporary directory for a file rename must be a limited length path: "%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%".
+  exit /b 41
+) >&2
+
+if not exist "%FROM_FILE_PATH%" goto XCOPY_FILE_TO_TMP_DIR_TO_RENAME
+
+:COPY_FILE_TO_TMP_DIR_TO_RENAME
+copy "%FROM_FILE_PATH%" "%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%" /B /Y || (
+  echo.%?~nx0%: error: could not copy into temporary directory: "%FROM_FILE_PATH%" -^> "%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%".
+  exit /b 50
+) >&2
+
+if not exist "\\?\%TO_FILE_DIR%\" (
+  mkdir "%TO_FILE_DIR%" 2>nul || robocopy.exe /create "%EMPTY_DIR_TMP%" "%TO_FILE_DIR%" >nul || (
+    echo.%?~nx0%: error: could not create a target file directory: "%TO_FILE_DIR%".
+    exit /b 51
+  ) >&2
+)
+
+call "%%CONTOOLS_ROOT%%/std/xcopy_file.bat" "%%COPY_WITH_RENAME_DIR_TMP%%" "%%TO_FILE_NAME%%" "%%TO_FILE_DIR%%" /Y /H || exit /b 52
+exit /b 0
+
+:XCOPY_FILE_TO_TMP_DIR_TO_RENAME
+call "%%CONTOOLS_ROOT%%/std/xcopy_file.bat" "%%FROM_FILE_DIR%%" "%%FROM_FILE_NAME%%" "%%COPY_WITH_RENAME_DIR_TMP%%" /Y /H || exit /b 53
+
+rename "%COPY_WITH_RENAME_DIR_TMP%\%FROM_FILE_NAME%" "%TO_FILE_NAME%" || (
+  echo.%?~nx0%: error: could not rename file in temporary directory: "%COPY_WITH_RENAME_DIR_TMP%\%FROM_FILE_NAME%" -^> "%TO_FILE_NAME%".
+  exit /b 60
+) >&2
+
+if not exist "%TO_FILE_PATH%" goto XCOPY_FILE_FROM_TMP_DIR
+
+copy "%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%" "%TO_FILE_PATH%" /B /Y || (
+  echo.%?~nx0%: error: could not copy a renamed file from temporary directory: "%FROM_FILE_PATH%" -^> "%COPY_WITH_RENAME_DIR_TMP%\%TO_FILE_NAME%".
+  exit /b 61
+) >&2
+exit /b 0
+
+:XCOPY_FILE_FROM_TMP_DIR
+call "%%CONTOOLS_ROOT%%/std/xcopy_file.bat" "%%COPY_WITH_RENAME_DIR_TMP%%" "%%TO_FILE_NAME%%" "%%TO_FILE_DIR%%" /Y /H || exit /b 62
+exit /b 0
