7986f3f3dee30c92fba88c8d6050d929017788c3
andry81 <andry@inbox.ru>
2021-01-08 05:51:45 +0000

    2021.01.07: * fixed: _install-fonts.bat: temporary directory cleanup after double execution * new: _install-fonts.bat: detect the reboot requirement state and print the message
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklebar/tacklebar/trunk@117 b6e1d463-c974-416b-a8a8-9e453b4b0c2d

--- _install-fonts.bat
+++ _install-fonts.bat
@@ -116,8 +116,6 @@ if defined FLAG (
 set "WINDOWS_FONTS_DIR=%SYSTEMROOT%\Fonts"
 set "PENDING_MOVE_ON_REBOOT_DIR_TMP=%SCRIPT_TEMP_CURRENT_DIR%\pending_move_on_reboot"
 
-set REBOOT_REQUIRED=0
-
 rem register some values
 "%SYSTEMROOT%\System32\reg.exe" add "HKCU\Software\Sysinternals\MoveFile" /v "EulaAccepted" /t REG_DWORD /d 0x00000001 /f
 
@@ -133,12 +131,12 @@ rem   Not empty directory can not be removed. You must process the whole directo
 rem
 
 call :CMD "%%CONTOOLS_UTILITIES_BIN_ROOT%%/sysinternals/movefile.exe" "%%PENDING_MOVE_ON_REBOOT_DIR_TMP%%" "" || (
-  echo.%?~nx0%: error: could not register file for pending delete operation: "%PENDING_MOVE_ON_REBOOT_DIR_TMP%\".
+  echo.%?~nx0%: error: could not register file for pending delete operation: "%PENDING_MOVE_ON_REBOOT_DIR_TMP%".
   exit /b 11
 )
 
 call :CMD "%%CONTOOLS_UTILITIES_BIN_ROOT%%/sysinternals/movefile.exe" "%%SCRIPT_TEMP_CURRENT_DIR%%" "" || (
-  echo.%?~nx0%: error: could not register file for pending delete operation: "%SCRIPT_TEMP_CURRENT_DIR%\".
+  echo.%?~nx0%: error: could not register file for pending delete operation: "%SCRIPT_TEMP_CURRENT_DIR%".
   exit /b 11
 )
 
@@ -156,6 +154,13 @@ call :CMD "%%SYSTEMROOT%%\System32\reg.exe" add "HKLM\SOFTWARE\Microsoft\Windows
 call :CMD "%%SYSTEMROOT%%\System32\reg.exe" add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Console\TrueTypeFont" /v "Terminus (TTF) for Windows"  /t REG_SZ /d "Terminus (TTF) for Windows" /f
 
 echo.
+
+rem Detect the reboot requirement state
+"%SYSTEMROOT%\System32\reg.exe" query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager" /v "PendingFileRenameOperations" 2>&1 >nul && (
+  echo.*******************************************************************************
+  echo.%?~nx0%: warning: to complete the fonts installation you must reboot!
+  echo.*******************************************************************************
+)
 
 echo.%?~nx0%: info: installation is complete.
 
@@ -175,6 +180,15 @@ call :CMD "%%CONTOOLS_UTILITIES_BIN_ROOT%%/sysinternals/movefile.exe" "%%PENDING
   echo.%?~nx0%: error: could not register file for pending move operation: "%PENDING_MOVE_ON_REBOOT_DIR_TMP%\%~2" -^> "%~f3\%~2".
   exit /b 11
 )
+
+rem CAUTION:
+rem   Need to remove the file if previous operation were ignored, otherwise the termporary directory won't be empty and so won't be deleted!
+rem
+call :CMD "%%CONTOOLS_UTILITIES_BIN_ROOT%%/sysinternals/movefile.exe" "%%PENDING_MOVE_ON_REBOOT_DIR_TMP%%\%%~2" "" || (
+  echo.%?~nx0%: error: could not register file for pending delete operation: "%PENDING_MOVE_ON_REBOOT_DIR_TMP%\%~2".
+  exit /b 11
+)
+
 exit /b 0
 
 :XCOPY_FILE
