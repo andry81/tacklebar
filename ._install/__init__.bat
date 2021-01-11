05da28309dfee6b7944d0959134c8d2d9b5ecb23
andry81 <andry@inbox.ru>
2021-01-11 08:12:02 +0000

    2021.01.11 * fixed: __init__.bat: variables load speedup * changed: __init__.bat: revert all directory initialization scripts back into simple not iterative implementation to load as fast as possible * changed: _config: configuration files downgraded to the lite parser format
    git-svn-id: svn+ssh://svn.code.sf.net/p/tacklebar/tacklebar/trunk@125 b6e1d463-c974-416b-a8a8-9e453b4b0c2d

--- ._install/__init__.bat
+++ ._install/__init__.bat
@@ -2,32 +2,4 @@
 
 if defined TACKLEBAR_PROJECT_ROOT_INIT0_DIR exit /b 0
 
-set LASTERRORLEVEL=0
-
-rem init script search logic
-for %%i in ("%~dp0..") do (
-  for %%j in ("__init__.bat" "__init__\__init__.bat") do (
-    call :INCLUDE %%i %%j && (
-      setlocal ENABLEDELAYEDEXPANSION & for %%i in (!LASTERRORLEVEL!) do (
-        endlocal
-        set "LASTERRORLEVEL="
-        exit /b %%i
-      )
-    )
-  )
-)
-
-set "LASTERRORLEVEL="
-
-exit /b 255
-
-:INCLUDE
-call :HAS_RECURSION "%%~1\%%~2" && exit /b 1
-if not exist "%~1\%~2" exit /b 1
-call "%%~1\%%~2"
-set LASTERRORLEVEL=%ERRORLEVEL%
-exit /b 0
-
-:HAS_RECURSION
-if /i "%~f1" == "%~f0" exit /b 0
-exit /b 1
+call "%%~dp0..\__init__\__init__.bat"
