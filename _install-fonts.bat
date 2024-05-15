956e87dd4ae24c30efb290284377b666b8bd983f
andry81 <andry@inbox.ru>
2024-05-15 17:22:56 +0300

    2024.05.13: * fixed: deploy/totalcmd/Profile/usercmd.ini.in: have to use `callfg.exe` instead of `callf.exe` in all `em_tkl_terminal_*` commands to avoid attach of `conhost.exe` to the `callf.exe` instead of `cmd.exe` and so avoid the saving console window settings into `HKEY_CURRENT_USER\Console\*_callf.exe` registry hive instead of `HKEY_CURRENT_USER\Console\..._cmd.exe` * refactor: _install*: minor code cleanup

--- _install-fonts.bat
+++ _install-fonts.bat
@@ -137,8 +137,7 @@ if defined FLAG (
   goto FLAGS_LOOP
 )
 
-if not defined NEST_LVL set NEST_LVL=0
-
+rem cast to integer
 set /A NEST_LVL+=1
 
 call "%%CONTOOLS_ROOT%%/std/allocate_temp_dir.bat" . "%%?~n0%%" || ( "set LAST_ERROR=255" & goto FREE_TEMP_DIR )
