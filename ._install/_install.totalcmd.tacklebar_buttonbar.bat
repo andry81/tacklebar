18fc23bce3e8ce802137664e5c524aff1e477bb4
andry81 <andry@inbox.ru>
2025-06-07 17:42:54 +0300

    2025.06.07: * fixed: _install: missed to update all `DEFAULT.bar` files if a default button bar file path record is absent in `wincmd.ini` file

--- ._install/_install.totalcmd.tacklebar_buttonbar.bat
+++ ._install/_install.totalcmd.tacklebar_buttonbar.bat
@@ -16,24 +16,37 @@ if "%DETECTED_TOTALCMD_PRODUCT_VERSION%" == "" (
 
 call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/check_vars.bat" DETECTED_TOTALCMD_WINCMD_CONFIG_FILE || exit /b
 
-echo;Searching for `Total Commander` buttonbar file...
+echo;Searching for `Total Commander` buttonbar file(s)...
 echo;
 
 set "TOTALCMD_BUTTONBAR_FILE_PATH="
 for /F "usebackq tokens=* delims="eol^= %%i in (`@"%SystemRoot%\System32\cscript.exe" /NOLOGO "%TACKLEBAR_PROJECT_EXTERNALS_ROOT%/tacklelib/vbs/tacklelib/tools/totalcmd/get_inifile_key.vbs" "%DETECTED_TOTALCMD_WINCMD_CONFIG_FILE%" "Buttonbar" "Buttonbar"`) do set "TOTALCMD_BUTTONBAR_FILE_PATH=%%i"
 
-if defined TOTALCMD_BUTTONBAR_FILE_PATH if exist "%TOTALCMD_BUTTONBAR_FILE_PATH%" goto INSTALL_TOTALCMD_BUTTONBAR_FILE
+rem list of ButtorBar files to update
+set "TOTALCMD_BUTTONBAR_FILE_PATH_LIST="
+if defined TOTALCMD_BUTTONBAR_FILE_PATH if exist "%TOTALCMD_BUTTONBAR_FILE_PATH%" set TOTALCMD_BUTTONBAR_FILE_PATH_LIST="%TOTALCMD_BUTTONBAR_FILE_PATH%" & goto INSTALL_TOTALCMD_BUTTONBAR_FILE_LIST
 
 rem search in the Total Commander installation directory
-if defined DETECTED_TOTALCMD_INSTALL_DIR if exist "%DETECTED_TOTALCMD_INSTALL_DIR%\DEFAULT.BAR" set "TOTALCMD_BUTTONBAR_FILE_PATH=%DETECTED_TOTALCMD_INSTALL_DIR%\DEFAULT.BAR" & goto INSTALL_TOTALCMD_BUTTONBAR_FILE
+if defined DETECTED_TOTALCMD_INSTALL_DIR if exist "%DETECTED_TOTALCMD_INSTALL_DIR%\DEFAULT.BAR" set TOTALCMD_BUTTONBAR_FILE_PATH_LIST=%TOTALCMD_BUTTONBAR_FILE_PATH_LIST% "%DETECTED_TOTALCMD_INSTALL_DIR%\DEFAULT.BAR"
+
+rem search in the APPDATA directory
+if defined APPDATA if exist "%APPDATA%\GHISLER\DEFAULT.BAR" set TOTALCMD_BUTTONBAR_FILE_PATH_LIST=%TOTALCMD_BUTTONBAR_FILE_PATH_LIST% "%APPDATA%\GHISLER\DEFAULT.BAR"
+
+rem search in the LOCALAPPDATA directory
+if defined LOCALAPPDATA if exist "%LOCALAPPDATA%\GHISLER\DEFAULT.BAR" set TOTALCMD_BUTTONBAR_FILE_PATH_LIST=%TOTALCMD_BUTTONBAR_FILE_PATH_LIST% "%LOCALAPPDATA%\GHISLER\DEFAULT.BAR"
+
+if defined TOTALCMD_BUTTONBAR_FILE_PATH_LIST goto INSTALL_TOTALCMD_BUTTONBAR_FILE_LIST
 
 (
-  echo;%?~%: error: `Total Commander` button bar configuration file is not found: "%TOTALCMD_BUTTONBAR_FILE_PATH%".
+  echo;%?~%: error: `Total Commander` button bar configuration file is not found: "%TOTALCMD_BUTTONBAR_FILE_PATH_LIST%".
   echo;
   exit /b 255
 ) >&2
 
-:INSTALL_TOTALCMD_BUTTONBAR_FILE
+:INSTALL_TOTALCMD_BUTTONBAR_FILE_LIST
+
+for %%i in (%TOTALCMD_BUTTONBAR_FILE_PATH_LIST%) do echo;  * %%i
+echo;
 
 set "TOTALCMD_CONFIG_UNINSTALLED_ROOT=%INSTALL_TO_DIR%\.uninstalled\totalcmd"
 
@@ -64,39 +77,8 @@ call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/mkdir_if_notexist.bat" "%%TOTALCMD_CONFIG_UN
   exit /b 255
 ) >&2
 
-:INSTALL_TOTALCMD_BUTTONBAR_INI
-if %WINDOWS_MAJOR_VER% GEQ 6 (
-  if %INSTALL_SINGLE_BUTTON_MENU% EQU 0 (
-    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/_common/buttonbar6432.ini.in"
-    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/_common/buttonbar32.ini.in"
-  ) else (
-    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/_common/buttonbar6432.ini.in"
-    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/_common/buttonbar32.ini.in"
-  )
-) else (
-  if %INSTALL_SINGLE_BUTTON_MENU% EQU 0 (
-    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/winxp/buttonbar6432.ini.in"
-    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/winxp/buttonbar32.ini.in"
-  ) else (
-    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/winxp/buttonbar6432.ini.in"
-    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/winxp/buttonbar32.ini.in"
-  )
-)
-set "TOTALCMD_BUTTONBAR_INOUT_FILE=%TOTALCMD_BUTTONBAR_FILE_PATH%"
-set "TOTALCMD_BUTTONBAR_CLEANUP_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/buttonbar_cleanup.ini"
-
-for /F "tokens=* delims="eol^= %%i in ("%TOTALCMD_BUTTONBAR_ADD_FILE%\.") do for /F "tokens=* delims="eol^= %%j in ("%%~dpi.") do set "TOTALCMD_BUTTONBAR_ADD_FILE=%%~fi" & set "TOTALCMD_BUTTONBAR_ADD_FILE_DIR=%%~fj" & set "TOTALCMD_BUTTONBAR_ADD_FILE_NAME=%%~nxi"
-for /F "tokens=* delims="eol^= %%i in ("%TOTALCMD_BUTTONBAR_INOUT_FILE%\.") do for /F "tokens=* delims="eol^= %%j in ("%%~dpi.") do set "TOTALCMD_BUTTONBAR_INOUT_FILE=%%~fi" & set "TOTALCMD_BUTTONBAR_INOUT_FILE_DIR=%%~fj" & set "TOTALCMD_BUTTONBAR_INOUT_FILE_NAME=%%~nxi
-for /F "tokens=* delims="eol^= %%i in ("%TOTALCMD_BUTTONBAR_CLEANUP_FILE%\.") do for /F "tokens=* delims="eol^= %%j in ("%%~dpi.") do set "TOTALCMD_BUTTONBAR_CLEANUP_FILE=%%~fi" & set "TOTALCMD_BUTTONBAR_CLEANUP_FILE_DIR=%%~fj" & set "TOTALCMD_BUTTONBAR_CLEANUP_FILE_NAME=%%~nxi"
-
-echo;Updating `Total Commander` button bar configuration file: "%TOTALCMD_BUTTONBAR_ADD_FILE%" -^> "%TOTALCMD_BUTTONBAR_INOUT_FILE%"...
+echo;Installing `Tacklebar` buttonbar files...
 echo;
-
-call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/xcopy_file.bat" "%%TOTALCMD_BUTTONBAR_INOUT_FILE_DIR%%" "%%TOTALCMD_BUTTONBAR_INOUT_FILE_NAME%%" "%%TOTALCMD_CONFIG_UNINSTALLED_DIR%%/%%TOTALCMD_BUTTONBAR_INOUT_FILE_NAME%%~%%RANDOM%%" /Y /D /H || (
-  echo;%?~%: error: backup of `Total Commander` button bar configuration file is failed.
-  echo;
-  exit /b 255
-) >&2
 
 set INSTALL_TOTALCMD_BUTTONBAR_BARE_FLAGS= -rep "{{WINVER_SUFFIX_SPEC}}" ""
 set GEN_CONFIG_TOTALCMD_BUTTONBAR_BARE_FLAGS= -rm "{{WINVER_SUFFIX_SPEC}}"
@@ -166,10 +148,53 @@ call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/call.bat" "%%CONTOOLS_BUILD_TOOLS_ROOT%%/gen
 
 echo;
 
+if %WINDOWS_MAJOR_VER% GEQ 6 (
+  if %INSTALL_SINGLE_BUTTON_MENU% EQU 0 (
+    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/_common/buttonbar6432.ini.in"
+    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/_common/buttonbar32.ini.in"
+  ) else (
+    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/_common/buttonbar6432.ini.in"
+    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/_common/buttonbar32.ini.in"
+  )
+) else (
+  if %INSTALL_SINGLE_BUTTON_MENU% EQU 0 (
+    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/winxp/buttonbar6432.ini.in"
+    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/multiple/winxp/buttonbar32.ini.in"
+  ) else (
+    set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/winxp/buttonbar6432.ini.in"
+    if %WINDOWS_X64_VER%0 EQU 0 set "TOTALCMD_BUTTONBAR_ADD_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/single/winxp/buttonbar32.ini.in"
+  )
+)
+
+set "TOTALCMD_BUTTONBAR_CLEANUP_FILE=%TACKLEBAR_PROJECT_ROOT%/deploy/totalcmd/Profile/buttonbar_cleanup.ini"
+
+for /F "tokens=* delims="eol^= %%i in ("%TOTALCMD_BUTTONBAR_ADD_FILE%\.") do for /F "tokens=* delims="eol^= %%j in ("%%~dpi.") do set "TOTALCMD_BUTTONBAR_ADD_FILE=%%~fi" & set "TOTALCMD_BUTTONBAR_ADD_FILE_DIR=%%~fj" & set "TOTALCMD_BUTTONBAR_ADD_FILE_NAME=%%~nxi"
+for /F "tokens=* delims="eol^= %%i in ("%TOTALCMD_BUTTONBAR_CLEANUP_FILE%\.") do for /F "tokens=* delims="eol^= %%j in ("%%~dpi.") do set "TOTALCMD_BUTTONBAR_CLEANUP_FILE=%%~fi" & set "TOTALCMD_BUTTONBAR_CLEANUP_FILE_DIR=%%~fj" & set "TOTALCMD_BUTTONBAR_CLEANUP_FILE_NAME=%%~nxi"
+
+for %%i in (%TOTALCMD_BUTTONBAR_FILE_PATH_LIST%) do set "TOTALCMD_BUTTONBAR_INOUT_FILE=%%~i" & call :INSTALL_TOTALCMD_BUTTONBAR_FILE
+
+goto INSTALL_TOTALCMD_BUTTONBAR_FILE_END
+
+:INSTALL_TOTALCMD_BUTTONBAR_FILE
+for /F "tokens=* delims="eol^= %%i in ("%TOTALCMD_BUTTONBAR_INOUT_FILE%\.") do for /F "tokens=* delims="eol^= %%j in ("%%~dpi.") do set "TOTALCMD_BUTTONBAR_INOUT_FILE=%%~fi" & set "TOTALCMD_BUTTONBAR_INOUT_FILE_DIR=%%~fj" & set "TOTALCMD_BUTTONBAR_INOUT_FILE_NAME=%%~nxi
+
+echo;Updating `Total Commander` button bar configuration file: "%TOTALCMD_BUTTONBAR_ADD_FILE%" -^> "%TOTALCMD_BUTTONBAR_INOUT_FILE%"...
+echo;
+
+call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/xcopy_file.bat" "%%TOTALCMD_BUTTONBAR_INOUT_FILE_DIR%%" "%%TOTALCMD_BUTTONBAR_INOUT_FILE_NAME%%" "%%TOTALCMD_CONFIG_UNINSTALLED_DIR%%/%%TOTALCMD_BUTTONBAR_INOUT_FILE_NAME%%~%%RANDOM%%" /Y /D /H || (
+  echo;%?~%: error: backup of `Total Commander` button bar configuration file is failed.
+  echo;
+  exit /b 255
+) >&2
+
 "%SystemRoot%\System32\cscript.exe" /NOLOGO "%TACKLEBAR_PROJECT_EXTERNALS_ROOT%/tacklelib/vbs/tacklelib/tools/totalcmd/install_totalcmd_buttonbar.vbs"%INSTALL_TOTALCMD_BUTTONBAR_BARE_FLAGS% "%TOTALCMD_BUTTONBAR_INOUT_FILE%" "%TOTALCMD_BUTTONBAR_INOUT_FILE%" "%TOTALCMD_BUTTONBAR_CLEANUP_FILE%" "%TOTALCMD_BUTTONBAR_ADD_FILE%" -1 True || (
   echo;%?~%: error: update of `Total Commander` button bar configuration file is aborted.
   echo;
   exit /b 255
 ) >&2
+
+echo;
+
+:INSTALL_TOTALCMD_BUTTONBAR_FILE_END
 
 exit /b 0
