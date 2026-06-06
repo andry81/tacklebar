9745aecfb4a1fb74b107c107b26136537fab2ad7
andry81 <andry@inbox.ru>
2026-06-06 19:09:32 +0300

    2026.06.06: * changed: __init__/__init__.bat: code cleanup

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -16,9 +16,6 @@ if defined NO_LOG set /A NO_LOG+=0
 rem Do not make a log output or stdio duplication into files
 if defined NO_LOG_OUTPUT set /A NO_LOG_OUTPUT+=0
 
-rem Do not change code page
-if defined NO_CHCP set /A NO_CHCP+=0
-
 if %TACKLEBAR_SCRIPTS_INSTALL%0 NEQ 0 goto IGNORE_COMMANDER_SCRIPTS_ROOT
 
 if not defined COMMANDER_SCRIPTS_ROOT (
@@ -56,10 +53,7 @@ call "%%~dp0canonical_path_if_ndef.bat" SVNCMD_PROJECT_EXTERNALS_ROOT           
 rem init immediate external projects
 
 if exist "%TACKLEBAR_PROJECT_EXTERNALS_ROOT%/contools/__init__/__init__.bat" (
-  rem disable code page change in nested __init__
-  set /A NO_CHCP+=1
   call "%%TACKLEBAR_PROJECT_EXTERNALS_ROOT%%/contools/__init__/__init__.bat" -no_load_user_config || exit /b
-  set /A NO_CHCP-=1
 )
 
 call "%%CONTOOLS_ROOT%%/std/get_windows_version.bat" || exit /b
@@ -80,20 +74,13 @@ if %NO_GEN%0 EQU 0 (
 
 rem init external projects
 
-if exist "%TACKLEBAR_PROJECT_EXTERNALS_ROOT%/tacklelib/__init__/__init__.bat" (
-  rem disable code page change in nested __init__
-  set /A NO_CHCP+=1
-  call "%%TACKLEBAR_PROJECT_EXTERNALS_ROOT%%/tacklelib/__init__/__init__.bat" -no_load_user_config || exit /b
-  set /A NO_CHCP-=1
-)
+call "%%CONTOOLS_ROOT%%/std/call_if_exist.bat" ^
+"%%TACKLEBAR_PROJECT_EXTERNALS_ROOT%%/tacklelib/__init__/__init__.bat" -no_load_user_config || exit /b
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/mkdir_if_notexist.bat" "%%PROJECT_OUTPUT_ROOT%%" || exit /b
 )
 
-if %NO_CHCP%0 EQU 0 (
-  if defined CHCP call "%%CONTOOLS_ROOT%%/std/chcp.bat" %%CHCP%%
-)
 
 call "%%TACKLEBAR_PROJECT_ROOT%%/tools/init_conemu.bat" || exit /b
 
