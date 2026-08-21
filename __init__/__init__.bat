e4a3fa10f30f2a89eae6dd2c489bb1a33c9c7f20
andry81 <andry@inbox.ru>
2026-08-21 17:50:41 +0300

    2026.08.21: * fixed: __init__/__init__.bat: initialization fixup

--- __init__/__init__.bat
+++ __init__/__init__.bat
@@ -75,7 +75,7 @@ if %NO_GEN%0 EQU 0 (
 rem init external projects
 
 call "%%CONTOOLS_ROOT%%/std/call_if_exist.bat" ^
-"%%TACKLEBAR_PROJECT_EXTERNALS_ROOT%%/tacklelib/__init__/__init__.bat" -no_load_user_config || exit /b
+  "%%TACKLEBAR_PROJECT_EXTERNALS_ROOT%%/tacklelib/__init__/__init__.bat" -no_load_user_config || exit /b
 
 if %NO_GEN%0 EQU 0 (
   call "%%CONTOOLS_BUILD_TOOLS_ROOT%%/mkdir_if_notexist.bat" "%%PROJECT_OUTPUT_ROOT%%" || exit /b
